b0e6b859807a284b97b336ce19b56e5663c06389
coffeee <71660795+acoffeee@users.noreply.github.com>
2026-05-11 08:52:56 -0400

    Implement user authentication function

--- backend/API/main.js
+++ backend/API/main.js
@@ -1,1 +1,18 @@
-stuff
+import data from '/backend/database/data.js';
+function clean_creds(username, password) {
+    const valid = /^[a-zA-Z0-9]+$/;
+
+    return valid.test(username) && valid.test(password);
+}
+export function authenticate(username, password) {
+    if(!clean_creds(username, password)) {
+        return [-1, "INVALID CREDENTIALS"];
+    }
+    for(const [user, pass] of Object.entries(data)) {
+        if(username === user && password === pass) {
+            return [1, "VALIDATED"];
+        }
+    }
+    return [0, "USERNAME OR PASSWORD MAYBE WRONG"];
+}
+export default authenticate;
