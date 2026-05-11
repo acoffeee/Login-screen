3e69a5afe567f790332bf4ece6e29122e73d64da
coffeee <71660795+acoffeee@users.noreply.github.com>
2026-05-11 08:54:44 -0400

    Integrate authentication function and handle login

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-
+import authenticate from '/backend/api/main.js';
 //note this is ai generated cause im LAZY
 const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 let running = false;
@@ -7,20 +7,35 @@ async function submit() {
     running = true;
     let username = document.getElementById("user");
     let password = document.getElementById("pass");
-    let terminal = document.getElementById("terminal");
+    let keyboard = document.getElementById("keyboard");
     username = username.value
     password = password.value;
+    let box = document.getElementById("loginbox");
     if(username === "" || password === "") {
-        let box = document.getElementById("loginbox");
         const errorMessage = document.createElement("p");
         errorMessage.classList.add("null-userpass");
         errorMessage.textContent ="Username or Password is invalid.";
         box.append(errorMessage);
-        console.log("condition met")
         await wait(1000);
         errorMessage.remove();
         running = false;
         return;
     }
+    let [ response_code, message ] = await authenticate(username, password);
+    console.log(response_code);
+    console.log(message);
+    if(response_code === 1) {
+        //login logic idr what im bouta do tbh
+        return;
+    } else {
+        const errorMessage = document.createElement("p");
+        errorMessage.classList.add("null-userpass");
+        errorMessage.textContent = message;
+        box.append(errorMessage);
+        await wait(1000);
+        errorMessage.remove();
+    }
     running = false;
 }
+
+let submit_EL = document.getElementById("submitDetails").addEventListener("click", submit);
